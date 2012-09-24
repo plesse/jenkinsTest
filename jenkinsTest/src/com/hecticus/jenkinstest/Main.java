215d7f04c2a07e737ebe4444319d801f3c07dc62
plesse <inaki@hecticus.com>
2012-09-24 11:48:13 -0430

    test 3

--- jenkinsTest/src/com/hecticus/jenkinstest/Main.java
+++ jenkinsTest/src/com/hecticus/jenkinstest/Main.java
@@ -16,7 +16,7 @@ import java.util.logging.Logger;
  */
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("jenkins test");
+		System.out.println("jenkins test!!!!!!!!!!!!!!");
 		FileWriter fstream = null;
 		try {
 			fstream = new FileWriter("out.txt");
